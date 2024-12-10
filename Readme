12338b44603b9ad0bd8a745935b9b2cb7485cfdc
Folasewa <abdulsalammaryam381@gmail.com>
2024-12-10 20:51:24 +0200

    Installing requirements

---
+++ Readme
@@ -0,0 +1,2 @@
+This is my first git interaction from my local computer.
+I will work on the mini project in my DS class using GIT commands from VSCode
--- Readme.md.m
+++
@@ -1,2 +0,0 @@
-This is my first git interaction from my local computer.
-I will work on the mini project in my DS class using GIT commands from VSCode
